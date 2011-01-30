ca77889fc39d34ac0ad394bd5853385a4eac34c4
t-p <thomas@ibpfeiffer.de>
2011-01-30 23:07:36 +0100

    added user model and show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Thomas Pfeiffer"
+  user.email                 "me@example.com"
+  user.password              "nopassword"
+  user.password_confirmation "nopassword"
+end
